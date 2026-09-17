c1c45d2dfc6d70fa06ab8bb5c4f21bf411132ae3
Muneeb-Akram6774 <muneeb.akram6774@gmail.com>
2026-09-17 12:18:57 +0500

    made changes in github oidc file

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+
+    tls = {
+      source = "hashicorp/tls"
+    }
   }
 }
 
--- bootstrap/outputs.tf
+++ bootstrap/outputs.tf
@@ -7,3 +7,23 @@ output "lock_table_name" {
   description = "DynamoDB table created for legacy Terraform state locking."
   value       = aws_dynamodb_table.terraform_locks.name
 }
+
+output "github_oidc_provider_arn" {
+  description = "ARN of the GitHub Actions OIDC provider."
+  value       = aws_iam_openid_connect_provider.github_actions.arn
+}
+
+output "github_plan_role_arn" {
+  description = "ARN of the GitHub Actions Terraform plan role."
+  value       = aws_iam_role.github_plan.arn
+}
+
+output "github_apply_role_arn" {
+  description = "ARN of the GitHub Actions Terraform apply role."
+  value       = aws_iam_role.github_apply.arn
+}
+
+output "github_prod_role_arn" {
+  description = "ARN of the GitHub Actions production Terraform apply role."
+  value       = aws_iam_role.github_prod.arn
+}
